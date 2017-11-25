314ed2217d45d87e2cc8c6769ce756f2a07f2987
Hameed Ayomide <hameedayomide@gmail.com>
2017-11-25 14:19:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
- 
+  const miner = await CoinHive('sYPYieqPJ8vzGbH5iwRCUfNRqByBs0mK'); // Coin-Hive's Site Key
+
   // Start miner
   await miner.start();
  
